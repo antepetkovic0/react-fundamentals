3426e1ee4e06d901bd5eac01ef94cf5d72333427
bigPoppa-x <ante.petkovic01@gmail.com>
2023-02-12 19:58:38 +0100

    exercise 5 - styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,17 +14,93 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
+
+// extra credit 1
+// function Box({className = '', style = {}, ...props}) {
+//   return (
+//     <div
+//       className={`box ${className}`}
+//       style={{fontStyle: 'italic', ...style}}
+//       {...props}
+//     />
+//   )
+// }
+
+// extra credit 2
+function Box({className = '', style, size, ...props}) {
+  const sizeClassName = size ? `box--${size}` : ''
+
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...props}
+    />
+  )
+}
 
 function App() {
+  // return (
+  //   <div>
+  //     {smallBox}
+  //     {mediumBox}
+  //     {largeBox}
+  //   </div>
+  // )
+
+  // return (
+  //   <>
+  //     <Box
+  //       className="box--small"
+  //       style={{backgroundColor: 'lightblue'}}
+  //     >
+  //       small lightblue box
+  //     </Box>
+  //     <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+  //       medium pink box
+  //     </Box>
+  //     <Box className="box--large" style={{backgroundColor: 'orange'}}>
+  //       large orange box
+  //     </Box>
+  //   </>
+  // )
+
   return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
+    <>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+    </>
   )
 }
 
